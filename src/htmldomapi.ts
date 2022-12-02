372d2e3c04993f01193374dcc61925d49fa4f91a
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-02 17:44:36 +0800

    Update htmldomapi.ts

--- src/htmldomapi.ts
+++ src/htmldomapi.ts
@@ -43,6 +43,7 @@ export interface DOMAPI {
   isDocumentFragment?: (node: Node) => node is DocumentFragment;
 }
 
+// 创建元素
 function createElement(
   tagName: any,
   options?: ElementCreationOptions
@@ -50,6 +51,7 @@ function createElement(
   return document.createElement(tagName, options);
 }
 
+// 创建元素2
 function createElementNS(
   namespaceURI: string,
   qualifiedName: string,
@@ -58,18 +60,22 @@ function createElementNS(
   return document.createElementNS(namespaceURI, qualifiedName, options);
 }
 
+// 创建文档片段
 function createDocumentFragment(): SnabbdomFragment {
   return parseFragment(document.createDocumentFragment());
 }
 
+// 创建文本节点
 function createTextNode(text: string): Text {
   return document.createTextNode(text);
 }
 
+// 创建注释节点
 function createComment(text: string): Comment {
   return document.createComment(text);
 }
 
+// 插入节点
 function insertBefore(
   parentNode: Node,
   newNode: Node,
@@ -92,17 +98,18 @@ function insertBefore(
   parentNode.insertBefore(newNode, referenceNode);
 }
 
+// 移除节点
 function removeChild(node: Node, child: Node): void {
   node.removeChild(child);
 }
-
+// 添加节点
 function appendChild(node: Node, child: Node): void {
   if (isDocumentFragment(child)) {
     child = parseFragment(child, node);
   }
   node.appendChild(child);
 }
-
+// 父节点
 function parentNode(node: Node): Node | null {
   if (isDocumentFragment(node)) {
     while (node && isDocumentFragment(node)) {
@@ -113,7 +120,7 @@ function parentNode(node: Node): Node | null {
   }
   return node.parentNode;
 }
-
+// 下一个兄弟节点
 function nextSibling(node: Node): Node | null {
   if (isDocumentFragment(node)) {
     const fragment = parseFragment(node);
@@ -127,31 +134,31 @@ function nextSibling(node: Node): Node | null {
   }
   return node.nextSibling;
 }
-
+// 获取标签名
 function tagName(elm: Element): string {
   return elm.tagName;
 }
-
+// 设置文本内容
 function setTextContent(node: Node, text: string | null): void {
   node.textContent = text;
 }
-
+// 获取文本内容
 function getTextContent(node: Node): string | null {
   return node.textContent;
 }
-
+// 是否元素节点
 function isElement(node: Node): node is Element {
   return node.nodeType === 1;
 }
-
+// 是否文本节点
 function isText(node: Node): node is Text {
   return node.nodeType === 3;
 }
-
+// 是否注释节点
 function isComment(node: Node): node is Comment {
   return node.nodeType === 8;
 }
-
+// 是否文档碎片节点
 function isDocumentFragment(node: Node): node is DocumentFragment {
   return node.nodeType === 11;
 }
